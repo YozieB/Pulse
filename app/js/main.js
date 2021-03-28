e9bdfe97b6d0b951f1cc1fadbcd5f4286e5951e1
Alex <Lev>
2021-03-28 16:57:40 +0300

    added stuff

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel__inner').slick({
         autoplay: true,
         responsive: [
             {
-             breakpoint: 992,
-             settings: {
-                 dots: true,
-                 arrows: false,
-             }
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                }
 
-        }
-    ]
+            }
+        ]
     });
-  });
+
+    $('.catalog__item-link').each(function(i){
+        $(this).on('click', function(e){
+            e.preventDefault();
+            $('.catalog__item-content').eq(i).toggleClass('catalog__item-content_active');
+            $('.catalog__item-list').eq(i).toggleClass('catalog__item-list_active');
+        })
+    })
+});
